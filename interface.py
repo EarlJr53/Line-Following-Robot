f9cd00208848666fde425195a2a4713ffa957300
Brooke Moss <me@brookem.dev>
2022-09-29 18:08:59 -0400

    Starter motor & control code

--- interface.py
+++ interface.py
@@ -1,1 +1,29 @@
+"""
 
+"""
+
+import serial
+
+
+COMPORT = "/dev/ttyACM0"  # Arduino port on Brooke's Ubuntu install
+BAUDRATE = 9600  # Set baud rate for serial connection
+
+# open the serial port
+serialPort = serial.Serial(COMPORT, BAUDRATE, timeout=1)
+
+complete = False
+started = False
+
+while not complete:
+    decision = input("Press 0 for STOP, 1 for SLOWER, and 2 for FASTER")
+    serialPort.write(bytes(decision))
+    if decision == "0":
+        complete = True
+    # if decision == "0" and not started:
+    #     serialPort.write(bytes("Start"))
+    #     started = True
+    # elif decision == "0" and not complete and started:
+    #     serialPort.write(bytes("Stop"))
+    #     complete = True
+    # else:
+    #     serialPort.write(bytes(decision))
--- rabbit/rabbit.ino
+++ rabbit/rabbit.ino
@@ -1,1 +1,63 @@
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
 
+int request = 0;
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_DCMotor motorL = AFMS.getMotor(1);
+Adafruit_DCMotor motorR = AFMS.getMotor(2);
+
+int sensorL;
+int sensorR;
+
+void setup()
+{
+    AFMS.begin();
+    //
+    // start the serial port
+    //
+    long baudRate = 9600;   // NOTE1: The baudRate for sending & receiving programs must match
+    Serial.begin(baudRate); // NOTE2: Set the baudRate to 115200 for faster communication
+    Serial.setTimeout(1);
+
+    // Only continue while serial connection is available
+    // This means nothing happens until the Python program is run
+    while (!Serial.available())
+    {
+    }
+
+    while (request == 0)
+    {
+        request = Serial.readString().toInt();
+    }
+
+    motorL->setSpeed(100*request)
+    motorR->setSpeed(100*request)
+}
+
+
+void loop()
+{
+    if (Serial.readString().toInt() == 0)
+    {
+      exit;
+    }
+    
+    if (!sensorL && !sensorR)
+    {
+      motorL->run(FORWARD);
+      motorR->run(FORWARD);
+    }
+    else if (!sensorL && sensorR)
+    {
+      motorL->run(FORWARD);
+      motorR->run(BACKWARD);
+    }
+    else if (sensorL && !sensorR)
+    {
+      motorL->run(BACKWARD);
+      motorR->run(FORWARD);
+    }
+
+}
